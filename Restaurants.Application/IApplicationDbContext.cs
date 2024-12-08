01c3750ad79d91bf1ae20d6e09bc725f64205a0e
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2024-12-09 00:42:53 +0200

    Create DbContexts Classes and ALL IS WELL and Voila working

---
+++ Restaurants.Application/IApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Application
+{
+	public interface IApplicationDbContext
+	{
+		public DbSet<Restaurant> Restaurants { get; set; }
+		public DbSet<Dish> Dishes { get; set; }
+
+		Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken());
+
+	}
+}
---
+++ Restaurants.Infrastructure/Persistance/Data/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Application;
+using Restaurants.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Infrastructure.Persistance.Data
+{
+	public sealed class ApplicationDbContext : DbContext, IApplicationDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+		}
+		public DbSet<Restaurant> Restaurants { get; set; } 
+		public DbSet<Dish> Dishes { get; set; } 
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			if (modelBuilder is null)
+				throw new ArgumentNullException(nameof(modelBuilder));
+
+			modelBuilder.Entity<Restaurant>()
+				.OwnsOne(A => A.Address);
+
+			modelBuilder.Entity<Restaurant>()
+				.HasMany(r => r.Dishes)
+				.WithOne(d => d.Restaurant)
+				.HasForeignKey(d => d.RestaurantId);
+
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+		}
+
+	}
+}
