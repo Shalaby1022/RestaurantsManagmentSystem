17947586a2b7967c9a24848dc310c52d1e382d97
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2024-12-08 22:01:55 +0200

    Setting Up the solution adhering to Clean Arctiecture Design and Models Entities and it's Ok for now!!!

---
+++ Restaurants.Domain/Entities/Dish.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Domain.Entities
+{
+	public class Dish
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public string Description { get; set; } = string.Empty;
+		public int? KiloCalories { get; set; }
+		public decimal Price { get; set; }
+
+
+
+		public Restaurant? Restaurant { get; set; }
+		public int RestaurantId { get; set; }
+	}
+}
---
+++ Restaurants.Domain/Entities/Restaurant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Domain.Entities
+{
+	public class Restaurant
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public string Description { get; set; } = string.Empty;
+		public string Category { get; set; } = string.Empty;
+		public bool HasDeleivery { get; set; }
+		public string? ContanctEmail { get; set; }
+		public string? ContactNumber { get; set; }
+
+		public Address Address { get; set; }
+		public List<Dish> Dishes { get; set; } = new List<Dish>();
+	}
+}
