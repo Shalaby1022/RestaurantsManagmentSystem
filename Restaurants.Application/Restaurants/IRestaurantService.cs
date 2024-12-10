2b855fdc689dacb395bb3b98c1f9921234b54ea5
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2024-12-10 13:33:51 +0200

    Intoducing Restaurants Service Both Interfcae and Concrete Implementation

---
+++ Restaurants.Application/Restaurants/IRestaurantService.cs
@@ -0,0 +1,11 @@
+﻿using Restaurants.Domain.Entities;
+
+namespace Restaurants.Application.Restaurants
+{
+	public interface IRestaurantService
+	{
+		Task<IEnumerable<Restaurant>> GetAllRestaurantAsync();
+		Task<Restaurant> GetRestaurantByIdAsync(int id);
+
+	}
+}
---
+++ Restaurants.Application/Restaurants/RestaurantService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Application.Restaurants
+{
+	public class RestaurantService : IRestaurantService
+	{
+		private readonly IRestaurantRepository _restaurantRepository;
+		private readonly ILogger<RestaurantService> _logger;
+
+		public RestaurantService(IRestaurantRepository restaurantRepository,
+								ILogger<RestaurantService> logger)
+		{
+			_restaurantRepository = restaurantRepository ?? throw new ArgumentNullException(nameof(restaurantRepository));
+			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		public async Task<IEnumerable<Restaurant>> GetAllRestaurantAsync()
+		{
+			var restuarntsRetrieved = await _restaurantRepository.GetAllRestaurantsAsync();
+			return restuarntsRetrieved;
+		}
+
+		public async Task<Restaurant> GetRestaurantByIdAsync(int id)
+		{
+			var getASpecificRestaurant = await _restaurantRepository.GetRestaurantByIdAsync(id);
+			return getASpecificRestaurant;
+		}
+	}
+}
