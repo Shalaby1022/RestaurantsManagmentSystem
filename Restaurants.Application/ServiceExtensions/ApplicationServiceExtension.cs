f8ac26086fc68c02d61beb5db1781fa50ded918a
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2024-12-10 13:33:24 +0200

    Service Extension Folder to manage all service regstiration in side Application project

---
+++ Restaurants.Application/ServiceExtensions/ApplicationServiceExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Restaurants.Application.Restaurants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Application.ServiceExtensions
+{
+	public static class ApplicationServiceExtension
+	{
+		public static void AddApplicationExtensionsToRgisterItInMainProgram(this IServiceCollection services,
+																					IConfiguration configuration)
+		{
+
+			services.AddScoped<IRestaurantService, RestaurantService>();
+
+		}
+	}
+}
