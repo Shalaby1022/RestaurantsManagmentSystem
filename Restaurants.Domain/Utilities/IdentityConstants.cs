55dffd5029c2f7617993e667e13b004d7fbf51a0
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2024-12-16 00:37:34 +0200

    Create a static class with constants for identity roles im gonna use onstead of hardcoding them every time i need it

---
+++ Restaurants.Domain/Utilities/IdentityConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Domain.Utilities
+{
+	public static class IdentityConstants
+	{
+		public const string Admin = "admin";
+		public const string User = "user";
+		public const string Owner = "owner";
+
+	}
+}
