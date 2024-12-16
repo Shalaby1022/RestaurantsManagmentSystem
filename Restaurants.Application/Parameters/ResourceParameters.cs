eaaf4efa428321b1b4ef58991f9b3a150423026c
Shalaby1022 <94740614+Shalaby1022@users.noreply.github.com>
2024-12-16 19:22:54 +0200

    Add ResourceParameters class for filtering and pagination support.

---
+++ Restaurants.Application/Parameters/ResourceParameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Application.Parameters
+{
+	public class ResourceParameters
+	{
+		// Filtering Seacrh Criteria prop
+		public string? SearchQuery { get; set; }
+
+		// Pagination Props
+		public int PageNumber { get; set; }
+		public int PageSize { get; set; } = 10;
+
+	}
+}
